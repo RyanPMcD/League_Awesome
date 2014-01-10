64f35bc9665745a52ed336617895302d6f7128ff
RyanPMcD <ryan.patrick.mcdonald@gmail.com>
2014-01-10 11:47:26 -0500

    changes to owner test and welcome index

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Owner do
-  it "has email of Ryan.Patrick.McDonald@gmail.com" do
+  it "has email of Ryan.Patrick.McDonald@gmail.com" do     # To leave a test "pending" you can just add an "x" in front of the "it" ("xit") or type out "pending" in the first line of the test. Very useful for debugging a lot of test one at a time.
   	owner = Owner.new(email: "Ryan.Patrick.McDonald@gmail.com")
-  	owner.email.should == "Ryan.Patrick.McDonald@gmail.com" 		# "should" is the "modifier" and the "==" is the "matcher". can also use the "be" matcher for more legible syntax
+  	owner.email.should == "Ryan.Patrick.McDonald@gmail.com" 		# "should" is the "modifier" and the "==" is the "matcher". can also use the "be" matcher for more legible syntax. Works very well with predicate methods
   end
+  
 end
